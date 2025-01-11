579213aca7746632ed41ce8c5abc4eeab49d918a
nch <nuchenghao666@163.com>
2025-01-11 22:16:04 +0800

    add my

--- start.sh
+++ start.sh
@@ -5,3 +5,7 @@
 python3 -u ./data/generate_data.py --config_path ./config/resnet50_cifar100/dir_5_seed_16_client_30/data.yaml
 # python3 -u ./server/fedavg.py --config_path ./config/resnet50_cifar100/dir_5_seed_16_client_30/fedavg.yaml
 python3 -u ./server/my.py --config_path ./config/resnet50_cifar100/dir_5_seed_16_client_30/loss_fixed_batch_global_loss5.yaml
+python3 -u ./server/my.py --config_path ./config/resnet50_cifar100/dir_5_seed_16_client_30/loss_fixed_batch_global_loss4.yaml
+python3 -u ./server/my.py --config_path ./config/resnet50_cifar100/dir_5_seed_16_client_30/loss_fixed_batch_global_loss3.yaml
+python3 -u ./server/my.py --config_path ./config/resnet50_cifar100/dir_5_seed_16_client_30/loss_fixed_batch_global_loss2.yaml
+python3 -u ./server/my.py --config_path ./config/resnet50_cifar100/dir_5_seed_16_client_30/loss_fixed_batch_global_loss1.yaml
