96d4cf75b64babc82e069a166966140364c297aa
nch <nuchenghao666@163.com>
2025-01-11 20:22:06 +0800

    add loss fixed batch global loss

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 # ---------------------- dir 0.5 ---------------------
 # ------------------ seed 16 ---------------------------
 python3 -u ./data/generate_data.py --config_path ./config/resnet50_cifar100/dir_5_seed_16_client_30/data.yaml
-python3 -u ./server/fedavg.py --config_path ./config/resnet50_cifar100/dir_5_seed_16_client_30/fedavg.yaml
+# python3 -u ./server/fedavg.py --config_path ./config/resnet50_cifar100/dir_5_seed_16_client_30/fedavg.yaml
+python3 -u ./server/my.py --config_path ./config/resnet50_cifar100/dir_5_seed_16_client_30/loss_fixed_batch_global_loss5.yaml
